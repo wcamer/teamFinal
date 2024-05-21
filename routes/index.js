d438bc70da18d49b09562bcb6cd6fc8c2b6543f4
wcamer <xeus.xiii@hotmail.com>
2024-05-21 02:30:24 -0600

    login and logout functionality added

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,25 @@ router.use('/api-docs', swaggerUI.serve)
 router.get('/api-docs', swaggerUI.setup(swaggerDoc))
 
 router.get('/', (req, res)=>{
-    res.send('Welcome to the team 4 Car Dealership App.  Available routes are "/vehicles, /dealerships, /employees, /customers, and /api-docs"')
+    let user
+    if(req.session.user === undefined){
+        user = "Visitor"
+    }else{
+        user = req.session.user.username
+    }
+    res.send(`Welcome ${user} to the team 4 Car Dealership App.  Available routes are "/vehicles, /dealerships, /employees, /customers, and /api-docs"`)
 })
 
 router.use('/vehicles', require('./vehicleRoute'))
 router.use('/dealerships', require('./dealershipRoute'))
 router.use('/employees', require('./employeeRoute'))
 router.use('/customers', require('./customerRoute'))
+router.use('/login', passport.authenticate('github', (req,res) =>{}))
+router.use('/logout', (req,res,next) =>{
+    req.logout((err) =>{
+        if(err) {return next(err)}
+        res.redirect('/')
+    })
+})
 
 module.exports = router
