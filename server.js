9a549de727279b4508f2ae6a44137993bab36fd4
wcamer <xeus.xiii@hotmail.com>
2024-05-21 02:30:09 -0600

    passport and session setup

--- server.js
+++ server.js
@@ -2,17 +2,39 @@ const express = require('express')
 const app = express()
 const mongodb = require('./database/db')
 const bodyParser = require('body-parser')
+const passport = require('./utilities/passport-setup')
+const session = require('express-session')
 
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000 
 
 
 app.use(bodyParser.json())
 
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUnitialized: true,
+}))
+
+app.use(passport.initialize())
+.use(passport.session())
+.use((req,res,next) =>{
+    res.setHeader('Access-Control-Allow0Methods', 'GET, POST, PUT, DELETE')
+next()
+})
 
 
 
 app.use('/' , require('./routes'))
+
+app.get('/github/callback', passport.authenticate('github', {
+    failureRedirect: '/api-docs', session: false}),
+    (req,res)=>{
+        req.session.user = req.user;
+        res.redirect('/')
+
+    })
 
 mongodb.init((err) =>{
     if(err) {
