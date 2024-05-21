a90bc6722ec3f6c3d983e256852365e594b90086
wcamer <xeus.xiii@hotmail.com>
2024-05-21 02:30:58 -0600

    passport setup

---
+++ utilities/passport-setup.js
@@ -0,0 +1,25 @@
+const passport = require('passport')
+const dotenv = require('dotenv').config()
+const ghStrat = require('passport-github2').Strategy
+
+passport.use(new ghStrat({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK_URL
+},
+(profile, done)=>{
+    return(null, profile)
+    })
+
+)
+
+
+passport.serializeUser((user,done)=>{
+    done(null,user)
+})
+
+passport.deserializeUser((user,done)=>{
+    done(null,user)
+})
+
+module.exports = passport
