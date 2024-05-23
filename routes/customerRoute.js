cd8409edcda50fa7b40fe1c84ad8bbdc759066c5
wcamer <xeus.xiii@hotmail.com>
2024-05-23 17:02:32 -0600

    added authentication check and user check to POST, PUT, and DELETE routes

--- routes/customerRoute.js
+++ routes/customerRoute.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 const customerCon = require('../controllers/customerController')
 const val = require('../utilities/customerValidation')
+const checker = require('../utilities/auth')
 
 router.get('/', customerCon.getAll)
 router.get('/:id', val.getCustomerRules(), val.getCustomerCheck, customerCon.getOne)
-router.post('/', customerCon.addCustomer)
-router.put('/:id', customerCon.updateCustomer)
-router.delete('/:id', customerCon.deleteCustomer)
+router.post('/', checker.userCheck, checker.authCheck,  customerCon.addCustomer)
+router.put('/:id', checker.userCheck, checker.authCheck, customerCon.updateCustomer)
+router.delete('/:id', checker.userCheck, checker.authCheck,  customerCon.deleteCustomer)
 
 module.exports = router;
