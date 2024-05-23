883d9e73cc22123e4d2a3bb116d477dafd6071c8
wcamer <xeus.xiii@hotmail.com>
2024-05-23 17:00:19 -0600

    added data validation rules and error handling for PUT, POST, and DELETE

--- utilities/employeeValidation.js
+++ utilities/employeeValidation.js
@@ -26,7 +26,7 @@ val.getEmployeeRules = () => {
         .withMessage("Please enter a valid employee id")
         .custom(
              async (_id, {req}) => {
-            _id = new mongoDbObjectId(req.params.id)
+            _id = new mDID(req.params.id)
             const employee = await mongodb.getDB().db().collection('employees').findOne({_id: _id})
             if(!employee) {
                 throw new Error("employee ID isn't Valid!!!" )
@@ -51,6 +51,187 @@ val.getEmployeeCheck = async (req, res, next) =>{
         console.log("It passed the getEmployeeCheck and associated rules.  Proceeding with retrieval operation")
     }
     next()
+}
+
+
+
+val.postEmployeeRules = () =>{
+    return[
+        body("firstName")
+        .trim()
+        .notEmpty()
+        .isLength({ min: 1})
+        .withMessage("Please provide a first name with at least 1 letter"),
+
+    body("lastName")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 1})
+    .withMessage("Please provide a last name with at least 1 letter"),
+ 
+    body("birthday")
+    .trim()
+    .notEmpty()
+    .matches(/^\d{4}-\d{2}-\d{2}$/)
+  
+    .withMessage("Please provide a birthday in the following format yyyy-mm-dd"),
+
+    body("phone")
+    .trim()
+    .isLength({min: 10})
+    .matches(/^\d{3}-\d{3}-\d{4}$/)
+    .withMessage("Please enter a phone number in the following format xxx-xxx-xxxx")
+    ,
+
+    body("email")
+    .trim()
+    .notEmpty()
+    .isEmail()
+    .normalizeEmail()
+    .isLength({ min: 4})
+    .withMessage("Please provide a valid email"),
+
+    body("startDate")
+        .trim()
+        .notEmpty()
+        .matches(/^\d{4}-\d{2}-\d{2}$/)
+        .withMessage("Please provide a valid date yyyy-mm-dd"),
+
+    body("username")
+    .trim()
+    .notEmpty()
+    .isLength({min: 5})
+    .withMessage("Please enter a user name with a length of 5")
+
+  
+
+    ]
+}
+
+val.postEmployeeCheck = async(req, res, next) => {
+    const _id = req.params.id
+    req.body = {_id}
+   let errors = []
+   errors =  validationResult(req)
+
+   if(!errors.isEmpty()){
+       console.log("Rule violation in postEmployeeCheck!!!  Operation canceled")
+       res.status(400).json({Message: errors})
+       return
+   }
+   else{
+       console.log("It passed the postEmployeeCheck and associated rules.  Proceeding with creation operation")
+   }
+   next()
+}
+
+val.putEmployeeRules = () =>{
+    return[
+        body("firstName")
+        .trim()
+        .notEmpty()
+        .isLength({ min: 1})
+        .withMessage("Please provide a first name with at least 1 letter"),
+
+    body("lastName")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 1})
+    .withMessage("Please provide a last name with at least 1 letter"),
+ 
+    body("birthday")
+    .trim()
+    .notEmpty()
+    .matches(/^\d{4}-\d{2}-\d{2}$/)
+  
+    .withMessage("Please provide a birthday in the following format yyyy-mm-dd"),
+
+    body("phone")
+    .trim()
+    .isLength({min: 10})
+    .matches(/^\d{3}-\d{3}-\d{4}$/)
+    .withMessage("Please enter a phone number in the following format xxx-xxx-xxxx")
+    ,
+
+    body("email")
+    .trim()
+    .notEmpty()
+    .isEmail()
+    .normalizeEmail()
+    .isLength({ min: 4})
+    .withMessage("Please provide a valid email"),
+
+    body("startDate")
+        .trim()
+        .notEmpty()
+        .matches(/^\d{4}-\d{2}-\d{2}$/)
+        .withMessage("Please provide a valid date yyyy-mm-dd"),
+
+    body("username")
+    .trim()
+    .notEmpty()
+    .isLength({min: 5})
+    .withMessage("Please enter a user name with a length of 5")
+
+  
+
+    ]
+}
+
+val.putEmployeeCheck = async(req, res, next) => {
+    const _id = req.params.id
+    req.body = {_id}
+   let errors = []
+   errors =  validationResult(req)
+
+   if(!errors.isEmpty()){
+       console.log("Rule violation in putEmployeeCheck!!!  Operation canceled")
+       res.status(400).json({Message: errors})
+       return
+   }
+   else{
+       console.log("It passed the putEmployeeCheck and associated rules.  Proceeding with update operation")
+   }
+   next()
+}
+
+
+
+val.deleteEmployeeRules = () => {
+    return [
+
+        body('_id')
+        .trim()
+        .isLength({min: 0, max: 24 })
+        .withMessage("Please enter a valid employee id")
+        .custom(
+             async (_id, {req}) => {
+            _id = new mDID(req.params.id)
+            const employee = await mongodb.getDB().db().collection('employees').findOne({_id: _id})
+            if(!employee) {
+                throw new Error("employee ID isn't Valid!!!" )
+            }
+        })
+    ]
+
+}
+
+
+val.deleteEmployeeCheck = async(req, res, next) => {
+    const _id = req.params.id
+    req.body = {_id}
+   let errors = []
+   errors =  validationResult(req)
+
+   if(!errors.isEmpty()){
+       console.log("Rule violation in deleteEmployeeCheck!!!  Operation canceled")
+       res.status(400).json({Message: errors})
+       return
+   }
+   else{
+       console.log("It passed the deleteEmployeeCheck and associated rules.  Proceeding with deletion operation")
+   }
+   next()
 }
 
 
