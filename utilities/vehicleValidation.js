07324807ef7fa856d8370b88dc6b4038a2b92733
wcamer <xeus.xiii@hotmail.com>
2024-05-23 16:59:50 -0600

    added data validation rules and error handling for POST, PUT, and DELETE routes

--- utilities/vehicleValidation.js
+++ utilities/vehicleValidation.js
@@ -26,8 +26,8 @@ val.getVehicleRules = () => {
         .withMessage("Please enter a valid vehicle id")
         .custom(
              async (_id, {req}) => {
-            _id = new mongoDbObjectId(req.params.id)
-            const vehicle = await mongodb.getDB().db().collection('cars').findOne({_id: _id})
+            _id = new mDID(req.params.id)
+            const vehicle = await mongodb.getDB().db().collection('vehicles').findOne({_id: _id})
             if(!vehicle) {
                 throw new Error("Vehicle ID isn't Valid!!!" )
             }
@@ -38,7 +38,7 @@ val.getVehicleRules = () => {
 
 val.getVehicleCheck = async (req, res, next) =>{
     const _id = req.params.id
-     req.body = {_id}
+    req.body = {_id}
     let errors = []
     errors =  validationResult(req)
 
@@ -54,4 +54,165 @@ val.getVehicleCheck = async (req, res, next) =>{
 }
 
 
+
+val.postVehicleRules = () => {
+    return [
+
+        body('make')
+        .trim()
+        .isLength({min: 1, max: 41})
+        .withMessage("Please enter a valid name"),
+
+        body('model')
+        .trim()
+        .isLength({min: 1, max: 41})
+        .withMessage("Please enter a valid model"),
+
+        body('color')
+        .trim()
+        .isLength({min: 1, max: 41})
+        .withMessage("Please enter a valid color"),
+                
+        body('trim')
+        .trim()
+        .isLength({min: 1, max: 41})
+        .withMessage("Please enter a valid trim level"),
+
+        body('year')
+        .trim()
+        .toInt()
+        .isLength({min: 4, max:4})
+        .isInt()
+        .withMessage("Please enter a 4 digit year"),
+        
+       body('mileage')
+        .trim()
+        .toInt()
+        .isLength({min: 1, max: 6})
+        .isInt()
+        .isNumeric()
+        .withMessage("Please enter a valid mileage.  At least 1 digit but no more than 6"),
+
+        body('price')
+        .trim()
+        .toInt()
+        .isLength({min: 1, max: 6})
+        .isInt()
+        .isNumeric()
+        .withMessage("Please enter a valid price without the dollar sign or commas"),
+
+    ]
+}
+
+val.postVehicleCheck = async(req,res,next) =>{
+
+   let errors = []
+   errors = validationResult(req)
+   if(!errors.isEmpty()){
+       console.log("Rule violation in postVehicleCheck!!!  Operation canceled")
+       res.status(400).json({Message: errors})
+       return
+   }else{
+       console.log("It passed the postVehicleCheck and associated rules.  Proceeding to creation operation")
+
+   }
+   next()
+}
+
+val.putVehicleRules = () => {
+    return[
+    
+
+            body('make')
+            .trim()
+            .isLength({min: 1, max: 41})
+            .withMessage("Please enter a valid name"),
+    
+            body('model')
+            .trim()
+            .isLength({min: 1, max: 41})
+            .withMessage("Please enter a valid model"),
+    
+            body('color')
+            .trim()
+            .isLength({min: 1, max: 41})
+            .withMessage("Please enter a valid color"),
+                    
+            body('trim')
+            .trim()
+            .isLength({min: 1, max: 41})
+            .withMessage("Please enter a valid trim level"),
+    
+            body('year')
+            .trim()
+            .toInt()
+            .isLength({min: 4, max:4})
+            .isInt()
+            .withMessage("Please enter a 4 digit year"),
+            
+           body('mileage')
+            .trim()
+            .toInt()
+            .isLength({min: 1, max: 6})
+            .isInt()
+            .isNumeric()
+            .withMessage("Please enter a valid mileage.  At least 1 digit but no more than 6"),
+    
+            body('price')
+            .trim()
+            .toInt()
+            .isLength({min: 1, max: 6})
+            .isInt()
+            .isNumeric()
+            .withMessage("Please enter a valid price without the dollar sign or commas"),
+    
+        
+    ]
+}
+
+val.putVehicleCheck = async(req, res, next)=>{
+    let errors = []
+    errors = validationResult(req)
+    if(!errors.isEmpty()){
+        console.log("Rule violation in putVehicleCheck!!!  Operation Canceled.")
+        res.status(400).json({Message: errors})
+        return
+    }else{
+        console.log("Passed putVehicleCheck and associated rules.  Proceeding with operation")
+    }
+    next()
+}
+
+val.deleteVehicleRules = () => {
+    return [
+
+        body('_id')
+        .trim()
+        // .notEmpty()
+        .isLength({min: 0, max: 24 })
+        .withMessage("Please enter a valid vehicle id")
+        .custom(
+             async (_id, {req}) => {
+            _id = new mDID(req.params.id)
+            const vehicle = await mongodb.getDB().db().collection('vehicles').findOne({_id: _id})
+            if(!vehicle) {
+                throw new Error("Vehicle ID isn't Valid!!!" )
+            }
+        })
+    ]
+}
+
+
+val.deleteVehicleCheck = async(req,res,next) =>{
+    let errors = []
+    errors = validationResult(req)
+    if(!errors.isEmpty()){
+        console.log("Rule violation in deleteVehicleCheck!!!  Operation Canceled.")
+        res.status(400).json({Message: errors})
+        return
+    }else{
+        console.log("Passed deleteVehicleCheck and associated rules.  Proceeding with operation")
+    }
+    next()
+}
 module.exports = val
