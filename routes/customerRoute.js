53c000dd7905fec7b1cf9977d6363d5607f0f119
camellecastro <camelleccastro@gmail.com>
2024-05-30 13:03:47 -0600

    Customer validation added on customer route

--- routes/customerRoute.js
+++ routes/customerRoute.js
@@ -5,8 +5,27 @@ const checker = require('../utilities/auth')
 
 router.get('/', customerCon.getAll)
 router.get('/:id', val.getCustomerRules(), val.getCustomerCheck, customerCon.getOne)
-router.post('/', checker.userCheck, checker.authCheck,  customerCon.addCustomer)
-router.put('/:id', checker.userCheck, checker.authCheck, customerCon.updateCustomer)
-router.delete('/:id', checker.userCheck, checker.authCheck,  customerCon.deleteCustomer)
+router.post(
+    '/',
+    checker.userCheck,
+    checker.authCheck, 
+    val.postCustomerRules(),
+    val.postCustomerCheck, customerCon.addCustomer)
+router.put(
+  "/:id",
+  checker.userCheck,
+  checker.authCheck,
+  val.putCustomerRules(),
+  val.putCustomerCheck,
+  customerCon.updateCustomer
+);
+router.delete(
+  "/:id",
+  checker.userCheck,
+  val.deleteCustomerRules(),
+  val.deleteCustomerCheck,
+  checker.authCheck,
+  customerCon.deleteCustomer
+);
 
 module.exports = router;
